d0dca717e7ea057481dd4e471b32c19d9ab9e919
junaid tahir <junaidtahir22@yahoo.com>
2023-04-30 13:35:59 +0500

    Ready to export excel

---
+++ firebase-sdk.js
@@ -0,0 +1,21 @@
+// Coordinator Sample SDK
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyDB2_pK8S302_0lCG4OqLh1JadwDDv2UzI",
+  authDomain: "dream-43bb8.firebaseapp.com",
+  databaseURL: "https://dream-43bb8.firebaseio.com",
+  projectId: "dream-43bb8",
+  storageBucket: "dream-43bb8.appspot.com",
+  messagingSenderId: "459951819490",
+  appId: "1:459951819490:web:991dd3af4ccf5ceb5b2cbb",
+  measurementId: "G-0D1VE0WHCB"
+};
+
+  // Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+var database = app.firestore();
+
+
+
+
+
