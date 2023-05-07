8775bf93113db5d8982c7caabae616108ae40e3e
junaid tahir <junaidtahir22@yahoo.com>
2023-05-07 14:47:10 +0500

    Export for Alp Burak

--- firebase-sdk.js
+++ firebase-sdk.js
@@ -1,14 +1,14 @@
 // Coordinator Sample SDK
   
 const firebaseConfig = {
-  apiKey: "AIzaSyDB2_pK8S302_0lCG4OqLh1JadwDDv2UzI",
-  authDomain: "dream-43bb8.firebaseapp.com",
-  databaseURL: "https://dream-43bb8.firebaseio.com",
-  projectId: "dream-43bb8",
-  storageBucket: "dream-43bb8.appspot.com",
-  messagingSenderId: "459951819490",
-  appId: "1:459951819490:web:991dd3af4ccf5ceb5b2cbb",
-  measurementId: "G-0D1VE0WHCB"
+  apiKey: "AIzaSyCghI2QoAaU9uuLi24eJx3iX4FxbMDmJrA",
+  authDomain: "acs-login-ebe96.firebaseapp.com",
+  databaseURL: "https://acs-login-ebe96-default-rtdb.firebaseio.com",
+  projectId: "acs-login-ebe96",
+  storageBucket: "acs-login-ebe96.appspot.com",
+  messagingSenderId: "693895008464",
+  appId: "1:693895008464:web:5ab76795daf9f298b94ed5",
+  measurementId: "G-F1SGQV2CZL"
 };
 
   // Initialize Firebase
